e95aa89c1496b05d94dd3ef3064ae7e0723bbdcc
Ty Stahnke <pstahnke@ucsd.edu>
2017-01-22 14:47:22 -0800

    Added sound effect, separated cannonballlauncher into it's own object

--- Assets/Scripts/CannonBallLauncher.cs
+++ Assets/Scripts/CannonBallLauncher.cs
@@ -5,22 +5,17 @@ using UnityEngine;
 public class CannonBallLauncher : MonoBehaviour {
 
     public GameObject cannonBallPrefab;
+    public AudioClip cannonSound;
     public Transform cannonBallSpanwerTrans;
     public float launchForce = 50000.0f;
 
-	// Use this for initialization
-	void Start () {
-
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
-
     public void Launch()
     {
+        //Instantiate cannon ball
         GameObject cannonBall = Instantiate(cannonBallPrefab, cannonBallSpanwerTrans.position, Quaternion.identity);
+        //Add force
         cannonBall.GetComponent<Rigidbody>().AddForce(transform.forward * launchForce);
+        //Play sound effect
+        gameObject.GetComponent<AudioSource>().PlayOneShot(cannonSound);
     }
 }
