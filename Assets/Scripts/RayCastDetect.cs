9b5a4d00b0d2c2b950359accede973efa176a217
Ty P Stahnke <pstahnke@UCSD.EDU>
2017-01-23 11:21:59 -0800

    Added rednered laserbeam. Added dwell. Need to implement mode changing scheme

--- Assets/Scripts/RayCastDetect.cs
+++ Assets/Scripts/RayCastDetect.cs
@@ -6,7 +6,9 @@ public class RayCastDetect : MonoBehaviour {
 
 	public void Launch() {
 
-        if (Physics.Raycast(transform.position, Vector3.forward, 100))
+        Vector3 fwd = transform.TransformDirection(Vector3.forward);
+
+        if (Physics.Raycast(transform.position, fwd, 100))
             print("There is something in front of the object!");
     }
 }
--- Assets/Scripts/TestInput.cs
+++ Assets/Scripts/TestInput.cs
@@ -14,45 +14,103 @@ public class TestInput : MonoBehaviour {
     private const int CANNON_MODE = 2;
     private const int MAX = 2;
 
+    private IEnumerator dwellRoutine;
+
     //init
     private void Start()
     {
-        rayCaster.SetActive(false);
+       rayCaster.SetActive(false);
+
+        dwellRoutine = Dwell();
+        StartCoroutine(dwellRoutine);
     }
 
-	// Update is called once per frame
+	//Keyboard debug
 	void Update () {
 		if(Input.GetKeyDown(KeyCode.Space))
         {
-            //Laser mode
-            if (mode == LASER_MODE)
-            {
-                rayCaster.GetComponent<RayCastDetect>().Launch();
-            }
-
-            //Cannon mode
-            else if (mode == CANNON_MODE)
-            {
-                cannonBallLauncher.GetComponent<CannonBallLauncher>().Launch();
-            }
+            Launch();
         }
 
         if(Input.GetKeyDown(KeyCode.Tab))
         {
-            //Change mode
-            if (mode < MAX)
-                mode++;
-            else
-                mode = 0;
-
-            //Update the mode settings
-            if (mode == NORMAL_MODE)
-                rayCaster.SetActive(false);
-            else if (mode == LASER_MODE)
-                rayCaster.SetActive(true);
-            else if (mode == CANNON_MODE)
-                rayCaster.SetActive(false);
-
+            switchMode();
         }
 	}
+
+    //Launch something
+    void Launch()
+    {
+        //Laser mode
+        if (mode == LASER_MODE)
+        {
+            rayCaster.GetComponent<RayCastDetect>().Launch();
+        }
+
+        //Cannon mode
+        else if (mode == CANNON_MODE)
+        {
+            cannonBallLauncher.GetComponent<CannonBallLauncher>().Launch();
+        }
+    }
+
+    //switch modes
+    void switchMode()
+    {
+        //Change mode
+        if (mode < MAX)
+            mode++;
+        else
+            mode = 0;
+
+        //Update the mode settings
+        if (mode == NORMAL_MODE)
+            rayCaster.SetActive(false);
+        else if (mode == LASER_MODE)
+            rayCaster.SetActive(true);
+        else if (mode == CANNON_MODE)
+            rayCaster.SetActive(false);
+    }
+    
+    //dwell coroutine
+    IEnumerator Dwell()
+    {
+        Quaternion startRot = gameObject.transform.rotation;
+        const float threshhold = 0.05f;
+        const float waitTime = 0.1f;
+        const float dwellTime = 2.0f;
+        float curTime = 0.0f;
+
+        while (true)
+        {
+            //update the time
+            yield return new WaitForSeconds(waitTime);
+            curTime += waitTime;
+
+            //didn't break threshhold since last update
+            if (Mathf.Abs(startRot.x - gameObject.transform.rotation.x) <= threshhold
+                && Mathf.Abs(startRot.y - gameObject.transform.rotation.y) <= threshhold
+                && Mathf.Abs(startRot.z - gameObject.transform.rotation.z) <= threshhold)
+            {
+                //if we didn't break threshhold for the total dwell time, active dwell
+                if (curTime >= dwellTime)
+                {
+                    //launch and reset
+                    //print("RESET: DWELL");
+                    Launch();
+                    curTime = 0.0f;
+                    startRot = gameObject.transform.rotation;
+                }
+            }
+
+            //broke threshhold
+            else
+            {
+                //reset
+                //print("RESET: BROKE THRESHHOLD");
+                curTime = 0.0f;
+                startRot = gameObject.transform.rotation;
+            }
+        }
+    }
 }
