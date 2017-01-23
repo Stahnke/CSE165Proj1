586658ff0e44413d0b5cda88bc1def63ef85d484
Ty P Stahnke <pstahnke@UCSD.EDU>
2017-01-23 11:43:10 -0800

    Changed TestInput -> InputManager

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TestInput : MonoBehaviour {
+public class InputManager : MonoBehaviour {
 
     public GameObject cannonBallLauncher;
     public GameObject rayCaster;
